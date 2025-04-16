401c01a60774767d0c9aa661db665cbf8c22c171
Riley-Edmunds <74948134+Riley-Edmunds@users.noreply.github.com>
2025-04-16 18:33:34 -0500

    Update batch_prefect.py

--- batch_prefect.py
+++ batch_prefect.py
@@ -5,9 +5,13 @@ from batch3 import batch3
 
 @flow
 def master_flow():
-    batch1()
-    batch2()
-    batch3()
+    batch1 = batch1().submit()
+    batch2 = batch2().submit()
+    batch3 = batch3().submit()
 
+    result1 = batch1.result()
+    result2 = batch2.result()
+    result3 = batch3.result()
+    
 if __name__ == "__main__":
     master_flow()
